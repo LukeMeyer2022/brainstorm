de4b5ad8c91732c97f050b158010cd7f7d9e3c60
LukeMeyer2022 <120164605+LukeMeyer2022@users.noreply.github.com>
2023-11-16 19:41:35 +0000

    update

--- Dashboard.py
+++ Dashboard.py
@@ -4,12 +4,28 @@ import pandas as pd
 import streamlit as st
 from streamlit.logger import get_logger
 
-st.sidebar()
-st.markdown("Tilte")
+# Title of the dashboard
+st.title("Your Dashboard Title")
 
-def brainstorm():
-  url = "https://github.com/LukeMeyer2022/brainstorm/DataMetsV5_Cleaned.xlsx"
-  df = pd.read_excel(url)
-  return df
+# Sidebar for interactive widgets
+with st.sidebar:
+    st.header("Sidebar")
+    # Add interactive widgets here, e.g., st.slider(), st.selectbox()
 
-brainstorm()
+@st.cache
+def load_data():
+    try:
+        url = "https://github.com/LukeMeyer2022/brainstorm/DataMetsV5_Cleaned.xlsx"
+        df = pd.read_excel(url)
+        return df
+    except Exception as e:
+        st.error(f"Error loading data: {e}")
+        return pd.DataFrame()
+
+# Load and display the data
+df = load_data()
+if not df.empty:
+    st.dataframe(df)
+
+# Additional code for more dashboard features
+
