89138847eee128438242123dd029d9fe66f67766
LukeMeyer2022 <120164605+LukeMeyer2022@users.noreply.github.com>
2023-11-16 19:37:10 +0000

    update

--- Dashboard.py
+++ Dashboard.py
@@ -8,7 +8,7 @@ st.sidebar()
 st.markdown("Tilte")
 
 def brainstorm():
-  url = "https://github.com/LukeMeyer2022/brainstorm.git"
+  url = "https://github.com/LukeMeyer2022/brainstorm/DataMetsV5_Cleaned.xlsx"
   df = pd.read_excel(url)
   return df
 
