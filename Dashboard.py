961c7bef43f483e3dcc79f6ced0e07878ca9d5db
LukeMeyer2022 <120164605+LukeMeyer2022@users.noreply.github.com>
2023-11-16 19:34:51 +0000

    update

--- Dashboard.py
+++ Dashboard.py
@@ -1,13 +1,16 @@
 
 # limitations under the License.
-
+import pandas as pd
 import streamlit as st
 from streamlit.logger import get_logger
 
-url = 
+st.sidebar()
+st.markdown("Tilte")
 
-
-
+def brainstorm():
+  url = "https://github.com/LukeMeyer2022/brainstorm.git"
+  df = pd.read_excel(url)
+  return df
 
 if __name__ == "__main__":
     run()
